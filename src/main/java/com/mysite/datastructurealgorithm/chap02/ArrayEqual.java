b9fa5c69881226b2ff32a74fe609f7f1caa918d0
meohin <meohin.meo@gmail.com>
2023-01-31 16:58:37 +0900

    Fix ArrayIndexOutOfBoundsException

--- src/main/java/com/mysite/datastructurealgorithm/chap02/ArrayEqual.java
+++ src/main/java/com/mysite/datastructurealgorithm/chap02/ArrayEqual.java
@@ -8,7 +8,7 @@ public class ArrayEqual {
             return false;
         }
 
-        for (int i : a) {
+        for (int i = 0; i < a. length; i++) {
             if (a[i] != b[i]) {
                 return false;
             }
