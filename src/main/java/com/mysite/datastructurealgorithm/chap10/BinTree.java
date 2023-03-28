48b096b444f571a872812f7cf937a0a5f4907c84
meohin <meohin.meo@gmail.com>
2023-03-28 14:50:14 +0900

    Add node class with 1 constructor and 3 methods

--- src/main/java/com/mysite/datastructurealgorithm/chap10/BinTree.java
+++ src/main/java/com/mysite/datastructurealgorithm/chap10/BinTree.java
@@ -1,4 +1,31 @@
 package com.mysite.datastructurealgorithm.chap10;
 
-public class BinTree {
+import java.util.Comparator;
+
+public class BinTree<K ,V> {
+    class Node<K, V> {
+        K key;
+        V data;
+        Node<K, V> left;
+        Node<K, V> right;
+
+        Node(K key, V data, Node<K, V> left, Node<K, V>right) {
+            this.key = key;
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return data;
+        }
+
+        void print() {
+            System.out.println(data);
+        }
+    }
 }
