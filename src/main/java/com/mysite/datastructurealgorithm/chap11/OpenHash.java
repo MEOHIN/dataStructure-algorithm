97dad4ac0e7c901295a9a96fb2153c9236de7b0a
meohin <meohin.meo@gmail.com>
2023-03-30 17:46:01 +0900

    Add bucket class with 1 constructor and 5 methods

--- src/main/java/com/mysite/datastructurealgorithm/chap11/OpenHash.java
+++ src/main/java/com/mysite/datastructurealgorithm/chap11/OpenHash.java
@@ -3,4 +3,38 @@ package com.mysite.datastructurealgorithm.chap11;
 public class OpenHash<K, V> {
     // 버킷 상태
     enum Status {OCCUPIED, EMPTY, DELETED}; // {데이터 저장, 비어 있음, 삭제 마침}
+
+    // 버킷
+    static class Bucket<K, V> {
+        private K key;
+        private V data;
+        private Status stat;
+
+        Bucket() {
+            stat = Status.EMPTY;
+        }
+
+        // 모든 필드에 값을 설정한다.
+        void set(K key, V data, Status stat) {
+            this.key = key;
+            this.data = data;
+            this.stat = stat;
+        }
+
+        public void setStat(Status stat) {
+            this.stat = stat;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return data;
+        }
+
+        public int hashCode() {
+            return key.hashCode();
+        }
+    }
 }
