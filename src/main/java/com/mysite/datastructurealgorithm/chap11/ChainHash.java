6dcec95a54ec7eae050b99dacc2d7cc8460ca0c3
meohin <meohin.meo@gmail.com>
2023-03-30 14:22:06 +0900

    Add node class with 1 constructor and 3 methods

--- src/main/java/com/mysite/datastructurealgorithm/chap11/ChainHash.java
+++ src/main/java/com/mysite/datastructurealgorithm/chap11/ChainHash.java
@@ -1,4 +1,28 @@
 package com.mysite.datastructurealgorithm.chap11;
 
 public class ChainHash<K, V> {
+    class Node<K, V> {
+        private K key;
+        private V data;
+        private Node<K, V> next;
+
+        Node(K key, V data, Node<K, V> next) {
+            this.key = key;
+            this.data = data;
+            this.next = next;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return data;
+        }
+
+        // 키 값의 해시 값을 반환한다.
+        public int hashCode() {
+            return key.hashCode();
+        }
+    }
 }
