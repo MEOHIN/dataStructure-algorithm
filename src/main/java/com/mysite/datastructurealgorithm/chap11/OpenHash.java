036bac80f131daede437d71d5c8296cc1abaf29a
meohin <meohin.meo@gmail.com>
2023-03-30 18:04:10 +0900

    Remove unused semicolon

--- src/main/java/com/mysite/datastructurealgorithm/chap11/OpenHash.java
+++ src/main/java/com/mysite/datastructurealgorithm/chap11/OpenHash.java
@@ -2,7 +2,7 @@ package com.mysite.datastructurealgorithm.chap11;
 
 public class OpenHash<K, V> {
     // 버킷 상태
-    enum Status {OCCUPIED, EMPTY, DELETED}; // {데이터 저장, 비어 있음, 삭제 마침}
+    enum Status {OCCUPIED, EMPTY, DELETED} // {데이터 저장, 비어 있음, 삭제 마침}
 
     // 버킷
     static class Bucket<K, V> {
